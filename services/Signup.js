d7b843b34a18dc5128f23705119b4b3c0ce7d373
vincenttse15 <vincenttse15@gmail.com>
2021-09-20 22:23:01 -0700

    insert user to db and password hash

--- services/Signup.js
+++ services/Signup.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs';
+
 function response(status, cookie, message) {
   const response = {
-    success,
+    success: status,
     cookie,
     message,
   }
@@ -8,12 +10,31 @@ function response(status, cookie, message) {
   return response;
 }
 
+function createUser(res, body, db) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(body.password, salt, function(err, hash) {
+      const user = {
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        salt: salt,
+        passwordHash: hash,
+      }
+
+      db.collection('users')
+        .insertOne(user)
+        .then((inserted) => console.log(inserted))
+        .catch(() => res.send(false, '', 'Sorry, there was an error creating your account.'));
+    })
+  });
+}
+
 export const signup = (res, body, db) => {
   db.collection('users')
     .findOne({email: body.email})
     .then((user) => {
       if (user) res.send(response(false, '', 'Email is already registered.'));
-      else res.send('success');
+      else createUser(res, body, db);
     })
     .catch(() => {
       res.send(response(false, '', 'Email is already registered.'));
