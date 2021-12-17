c9375ddfe95716a5784b21382da72907efacb895
vincenttse15 <vincenttse15@gmail.com>
2021-12-17 04:23:13 -0800

    chat server updates

--- chatServer.js
+++ chatServer.js
@@ -8,8 +8,11 @@ const wss = new WebSocketServer({ path: '/socket', port: 4004 });
 
 wss.on('connection', (ws) => {
   console.log('connected');
+  
+  ws.on('close', () => {
+    console.log('disconnected');
+  });
 });
-
 
 client.subscribe('notifications');
 console.log('chat server online');
