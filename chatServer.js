5df44c41e5a6213d51ffe0860169d832bf4536b8
vincenttse15 <vincenttse15@gmail.com>
2021-12-19 00:57:47 -0800

    websocket

--- chatServer.js
+++ chatServer.js
@@ -4,13 +4,17 @@ import { WebSocketServer } from "ws";
 const redisHost = 'redis';
 const client = redis.createClient({ host: redisHost });
 
-const wss = new WebSocketServer({ path: '/socket', port: 4004 });
+const wss = new WebSocketServer({ port: 4004 });
+const users = new Map();
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
   console.log('connected');
-  
+  users.set(req.url.slice(1), ws);
+
   ws.on('close', () => {
     console.log('disconnected');
+    users.delete(req.url.slice(1));
+    console.log(users);
   });
 });
 
