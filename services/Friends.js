3408414a789ad80a7bbe20f0733650e18ae1a67c
vincenttse15 <vincenttse15@gmail.com>
2021-12-22 04:56:11 -0800

    friend request updates

--- services/Friends.js
+++ services/Friends.js
@@ -1,3 +1,5 @@
+import { ObjectId } from "bson";
+
 const requestResponse = (success, message) => {
   const response = {
     success: success,
@@ -9,15 +11,37 @@ const requestResponse = (success, message) => {
 
 export const sendFriendRequest = (res, body, db) => {
   db.collection("users")
-    .findOne({email: body.email}, function(err, user) {
-      if (user) {
-        const data = {
-          success: true,
-        }
+    .findOne({ email: body.email }, function (err, to) {
+      if (to) {
+        db.collection("sessions")
+          .findOne({ "session.uuid": body.cookie }, function (err, session) {
+            if (session) {
+              db.collection("users")
+                .findOne({ _id: new ObjectId(session.session.userId) }, function (err, from) {
+                  if (from) {
+                    const friendRequest = {
+                      from: from._id.toString(),
+                      to: to._id.toString(),
+                    };
 
-        res.send(data);
+                    db.collection("FriendRequests")
+                      .insertOne(friendRequest)
+                      .then((inserted) => {
+                        res.send(requestResponse(true, "Friend request sent."));
+                      })
+                      .catch(() => {
+                        res.send(requestResponse(false, "Sorry, there was an error sending the friend request."));
+                      })
+                  } else {
+                    res.send(requestResponse(false, "Error, please try again."));
+                  }
+                });
+            } else {
+              res.send(requestResponse(false, "Error finding session, please try relogging."));
+            }
+          })
       } else {
-        res.send(requestResponse(false, "User does not exist."));
+        res.send(requestResponse(false, "User with that email does not exist."));
       }
     })
 };
